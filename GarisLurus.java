479ecb3b5b1ce117ae1f3c570939e85decbf516c
damaisaputralaoli <73173498+damaisaputralaoli@users.noreply.github.com>
2020-11-29 12:00:45 +0700

    Add files via upload

---
+++ GarisLurus.java
@@ -0,0 +1,13 @@
+package gradien_garis_lurus;
+
+/**
+ * @author 
+ * NAMA     : Damai Saputra Laoli
+ * KELAS    : IF-1
+ * NIM      : 10119012
+ */
+
+public interface GarisLurus {
+    public int hitungGradien();
+}
+
